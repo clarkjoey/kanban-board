ba5a1d80323c0b024a479cfe1153b533ce73c413
clarkjoey <joeclark3516@gmail.com>
2023-10-25 19:17:39 -0400

    fixed the create task and style

--- src/KanbanBoard.js
+++ src/KanbanBoard.js
@@ -4,24 +4,28 @@ const initialData = {
   todo: {
     title: 'To Do',
     items: [
-      { id: 'task-1', content: 'Eat Food' },
-      { id: 'task-2', content: 'Commit to Git' },
-      { id: 'task-3', content: 'Workout' },
+      { id: 'task-1', content: 'Work' },
+      { id: 'task-2', content: 'Eat' },
+      { id: 'task-3', content: 'Sleep' },
     ],
+    inputId: 'input-1',
   },
   inProgress: {
     title: 'In Progress',
     items: [],
+    inputId: 'input-2',
   },
   done: {
     title: 'Done',
     items: [],
+    inputId: 'input-3',
   },
 };
 
 const KanbanBoard = () => {
   const [data, setData] = useState(initialData);
   const [draggedItem, setDraggedItem] = useState(null);
+  const [newTaskText, setNewTaskText] = useState({});
 
   const handleDragStart = (e, item, columnId) => {
     setDraggedItem({ item, columnId });
@@ -59,30 +63,67 @@ const KanbanBoard = () => {
     handleDragEnter(e, columnId);
   };
 
+  const handleNewTaskChange = (e, inputId) => {
+    setNewTaskText({
+      ...newTaskText,
+      [inputId]: e.target.value,
+    });
+  };
+
+  const handleCreateTask = (columnId, inputId) => {
+    if (newTaskText[inputId].trim() === '') return;
+
+    const newData = { ...data };
+    const newTask = { id: `task-${Date.now()}`, content: newTaskText[inputId] };
+    newData[columnId].items.push(newTask);
+    setData(newData);
+
+    // Clear the input field
+    setNewTaskText({
+      ...newTaskText,
+      [inputId]: '',
+    });
+  };
+
   return (
     <div className="kanban-board">
-      {Object.keys(data).map((columnId) => {
-        const column = data[columnId];
+      {Object.keys(data).map((columnKey) => {
+        const column = data[columnKey];
         return (
           <div
-            key={columnId}
+            key={columnKey}
             className="kanban-column"
             onDragOver={handleDragOver}
-            onDrop={(e) => handleDrop(e, columnId)}
+            onDrop={(e) => handleDrop(e, columnKey)}
           >
             <h2>{column.title}</h2>
+            <div className="kanban-new-task">
+              <input
+                id={column.inputId}
+                type="text"
+                placeholder="New task..."
+                value={newTaskText[column.inputId] || ''}
+                onChange={(e) => handleNewTaskChange(e, column.inputId)}
+              />
+              <button
+                onClick={() => handleCreateTask(columnKey, column.inputId)}
+              >
+                Create
+              </button>
+            </div>
             <div className="kanban-task-list">
               {column.items.map((item) => (
                 <div
                   key={item.id}
                   className="kanban-task"
                   draggable
-                  onDragStart={(e) => handleDragStart(e, item, columnId)}
+                  onDragStart={(e) => handleDragStart(e, item, columnKey)}
                 >
                   {item.content}
                 </div>
               ))}
             </div>
+            
           </div>
         );
       })}
