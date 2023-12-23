88ec2690939345fadeb70fc0467b1f4f3d12db30
clarkjoey <joeclark3516@gmail.com>
2023-12-22 20:48:43 -0500

    add logo to app

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,16 @@ import KanbanBoard from './KanbanBoard';
 
 function App() {
   return (
-    <div className="App">
-      <KanbanBoard />
-    </div>
+    <>
+      <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <img src="/flow-transparent-logo.png" width="64" height="auto" class="d-inline-block align-text-top" alt="App Logo" />
+        </div>
+      </nav>
+      <div className="App">
+        <KanbanBoard />
+      </div>
+    </>
   );
 }
 
