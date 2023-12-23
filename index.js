fcf11fc27658bbe8acba5e9b16eef53452b2d089
clarkjoey <joeclark3516@gmail.com>
2023-12-23 17:31:16 -0500

    added login

--- index.js
+++ index.js
@@ -29,6 +29,11 @@ app.use(session({
 app.use(
   auth({
     authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL
   })
 );
 
